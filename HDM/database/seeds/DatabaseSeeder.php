e259f2fdaff7ee0f542f747009ebf41ebf5f2b3f
Fabrizio <fabriziolucenti@gmail.com>
2019-08-14 11:48:57 +0200

    Update DatabaseSeeder.php

--- HDM/database/seeds/DatabaseSeeder.php
+++ HDM/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(AdminTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
     }
 }
