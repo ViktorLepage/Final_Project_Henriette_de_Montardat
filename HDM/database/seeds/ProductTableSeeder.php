47f3868277c383a4eee672b065d3b4583964f822
gokdag <gokdag.goktepe@gmail.com>
2019-08-14 09:42:37 +0200

    Seeder Updates
    hdm_user seeder update

--- HDM/database/seeds/ProductTableSeeder.php
+++ HDM/database/seeds/ProductTableSeeder.php
@@ -11,6 +11,22 @@ class ProductTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 0; $i < 11; $i++) {
+            DB::table('hdm_products')->insert([
+                'product_type' => 'MyType' . $i,
+                'product_name' => Str::random(10),
+                'price' => Int::random(5),
+                'img' => Str::random(5),
+                'categoryID' => Int::random(1, 6),
+            ]);
+        }
     }
 }
+$table->bigIncrements('product_id');
+$table->string('product_type');
+$table->string('product_name');
+$table->integer('price');
+$table->string('img');
+$table->integer('categoryID');
+$table->foreign('categoryID')->references('category_id')->on('hdm_categories');
+$table->timestamps();
