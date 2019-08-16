71db8be8909fa1efc866ebce1501207648f7cee1
bihsen <ihsenbiouss@gmail.com>
2019-08-16 16:55:09 +0200

    Js-implementation

--- HDM/resources/views/auth/login.blade.php
+++ HDM/resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                                     <div class="form-group row">
                                         <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                                        <div class="col-md-6">
+                                        <div id="logInDiv" class="col-md-6">
                                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                             @error('email')
@@ -71,6 +71,17 @@
                     </div>
                 </div>
             </div>
+
+
+
+
+
+
+
+
+
+
+
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-8">
@@ -78,14 +89,17 @@
                             <div class="card-header">{{ __('Register') }}</div>
 
                             <div class="card-body">
-                                <form method="POST" action="{{ route('register') }}">
+
+
+                                <!-- -----------Register Form-------- -->
+                                <form id="registerForm" method="POST" action="{{ route('register') }}">
                                     @csrf
 
                                     <div class="form-group row">
                                         <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                            <input id="registerName" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                             @error('name')
                                                 <span class="invalid-feedback" role="alert">
@@ -98,7 +112,7 @@
                                         <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
+                                            <input id="registerSurname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
 
                                             @error('surname')
                                                 <span class="invalid-feedback" role="alert">
@@ -112,7 +126,7 @@
                                         <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                            <input id="registerEmail" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                             @error('email')
                                                 <span class="invalid-feedback" role="alert">
@@ -124,9 +138,9 @@
 
                                     <div class="form-group row">
                                         <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
+                                            <!--  registerPassword -->
                                         <div class="col-md-6">
-                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                            <input id="registerPassword" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                             @error('password')
                                                 <span class="invalid-feedback" role="alert">
@@ -137,7 +151,7 @@
                                     </div>
 
                                     <div class="form-group row">
-                                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                        <label for="registerPassword-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                                         <div class="col-md-6">
                                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -158,6 +172,7 @@
                 </div>
             </div>
 </div>
+<script type="text/javascript" src="{{ URL::asset('js/loginScript.js') }}"></script>
 
 @endsection
 
