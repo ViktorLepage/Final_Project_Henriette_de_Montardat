255ab47299190f4ef7c442f51e7365109646fd1e
ZoltanSzabo99 <50532396+ZoltanSzabo99@users.noreply.github.com>
2019-08-19 22:43:41 +0200

    CSS files of boutique and collection

--- HDM/resources/views/boutique.blade.php
+++ HDM/resources/views/boutique.blade.php
@@ -11,65 +11,9 @@
         </form>
 @endforeach -->
 
-<style>
-#carousel-image{
-    border-style: dotted dashed solid double;
-    box-shadow: 0 15px 20px rgba(0, 0, 0, 0.3);
-}
 
-#carousel-inner{
-    text-align: center;
-}
-.carousel-item{
-    width: 500px;
-    height: 380px;
-}
-
-#carouselController{
-    background-color : #403ead;
-    height : 50%;
-}
-
-.carousel-item img{
-    height: 200px;
-}
-
-.carousel-control-prev-icon{
-    background-color : #403ead;
-}
-
-.button1 {
-  height: 40px;
-  width: 150px;
-  border: 2px solid #403ead;
-  margin: 20px 20px 20px 20px;
-
-  text-transform: uppercase;
-  text-decoration: none;
-  font-size: .8em;
-  letter-spacing: 1.5px;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-}
-
-.button2:hover {
-    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
-}
-
-.button {
-  display: inline-flex;
-
-}
-
-
-
-
-
-
-
-</style>
 <head>
+    <link rel="stylesheet"  href="css/boutique.css"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- HDM/resources/views/collection.blade.php
+++ HDM/resources/views/collection.blade.php
@@ -8,61 +8,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="">
 <link rel="stylesheet"  href="css/lightslider.css"/>
-<style>
-ul{
-    list-style: none outside none;
-    padding-left: 0;
-    margin: 0;
-}
-.mainslider {
-    margin-bottom: 60px;
+<link rel="stylesheet"  href="css/collection.css"/>
 
-}
-.content-slider li{
-    box-shadow: 0 15px 20px rgba(0, 0, 0, 0.3);
-    text-align: center;
-    color: #FFF;
-}
-.content-slider h3 {
-    margin: 0;
-    padding: 70px 0;
-}
-.mainslider{
-    width: 800px;
-    margin:auto;
-    padding-top:1%;
-}
-
-img {
-    width:200px;
-    height:200px;
-}
-
-.itemcard{
-    padding-top: 5px;
-    padding-bottom: 5px;
-}
-
-.button1 {
-  height: 40px;
-  width: 150px;
-  border: 2px solid #403ead;
-  margin: 20px 20px 20px 20px;
-  text-transform: uppercase;
-  text-decoration: none;
-  font-size: .8em;
-  letter-spacing: 1.5px;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-}
-
-.button2:hover {
-    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
-}
-
-
-</style>
 <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->'
 <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
 <script src="js/lightslider.js"></script>
