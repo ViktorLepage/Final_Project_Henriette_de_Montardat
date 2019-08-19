9f0e5cb250f027bc5942ffdbc4f4a3eb418b9201
bihsen <ihsenbiouss@gmail.com>
2019-08-19 16:06:59 +0200

    header-footer update
    login page update footer/header

--- HDM/resources/views/Layouts/headerTemplate.blade.php
+++ HDM/resources/views/Layouts/headerTemplate.blade.php
@@ -5,34 +5,57 @@
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
 <link rel="stylesheet" href="{{asset('css/header.css')}}">
+<link rel="stylesheet" href="{{asset('css/header.css')}}">
+
 
 
   
   <header>
-    <nav  class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar w/ text</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
+    <nav   class="  d-flex justify-content-between navbar navbar-expand-lg navbar-light wg-light">
+      <div class="col-md-6" > 
+        
+        <div class=" d-flex p-4" id="logo">
+          <img src="images/Henriette-Logo.jpg" alt="logo">
+        </div>
 
-                    <div class="collapse navbar-collapse" id="navbarText">
-                      <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Features</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login/SignUp') }}</a>
-                        </li>
-                      </ul>
-                     
-                    </div>
-                  </nav>
+      </div>
+      <div class=" d-flex p-4">
+
+        <div class=" d-flex justify-content-end">
+  
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+    
+                        <div class="collapse navbar-collapse" id="navbarText">
+                          <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                              <a class="nav-link" href="#">Paged'acceuil <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Collections</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Coup de coeur</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Connection') }}</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Nouveau compte</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">CONTACT</a>
+                            </li>
+                          
+                          </ul>
+                         
+                        </div>
+  
+        </div>
+
+      </div>
+    </nav>
     </header>
     
 
