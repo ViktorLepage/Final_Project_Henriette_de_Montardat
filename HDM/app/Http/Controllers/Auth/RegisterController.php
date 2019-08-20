3567aac8561944080cd859e7fdc496b5adda7f89
Gökdağ Göktepe <gokdag.goktepe@gmail.com>
2019-08-20 17:00:42 +0200

    Some tests for auth

--- HDM/app/Http/Controllers/Auth/RegisterController.php
+++ HDM/app/Http/Controllers/Auth/RegisterController.php
@@ -65,8 +65,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            // 'name' => $data['name'],
-            // 'surname' => $data['surname'],
+            'name' => $data['name'],
+            'surname' => $data['surname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
