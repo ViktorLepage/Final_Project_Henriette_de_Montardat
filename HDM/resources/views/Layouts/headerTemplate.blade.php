bc0a087fd765f5ba8414d357f38f009a532389d3
Gökdağ Göktepe <gokdag.goktepe@gmail.com>
2019-08-20 16:42:44 +0200

    Navbar item added

--- HDM/resources/views/Layouts/headerTemplate.blade.php
+++ HDM/resources/views/Layouts/headerTemplate.blade.php
@@ -45,6 +45,9 @@
                                 <a class ="nav-link" href="{{ URL::to('logout') }}">Logout</a>
                             </li>
                             <li class="nav-item">
+                                <a class ="nav-link" href="/myaccount">My Account</a>
+                            </li>
+                            <li class="nav-item">
                               <a class="nav-link" href="/contact">CONTACT</a>
                             </li>
 
