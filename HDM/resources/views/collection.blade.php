3bc0ff8fd9c88b36b5f3cb5aa65c52ce43f25a5e
Fabrizio <fabriziolucenti@gmail.com>
2019-08-20 10:56:52 +0200

    insert - update
    updated button to edit products

--- HDM/resources/views/collection.blade.php
+++ HDM/resources/views/collection.blade.php
@@ -42,10 +42,10 @@
                 <li class="itemcard">
                     <img  id="carousel-image" src="{{ URL::asset($product->image)}}" >
                     <p>{{$product->product_name}}</p>
-                    <form action="" method="GET">
+                <form action="/admin/product/{{$product->id}}/edit" method="GET">
                       @csrf
                       <input name="addToCart" value="{{$product->id}}" type="hidden">
-                     <button class ="button1 button2" type="submit">add</button>
+                     <button class ="button1 button2" type="submit">edit</button>
                     </form>
                 </li>
             @endforeach
