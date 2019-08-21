4c962705b3abf1a0158a1012e633f2264e345b19
ZoltanSzabo99 <50532396+ZoltanSzabo99@users.noreply.github.com>
2019-08-21 15:59:30 +0200

    Css
    CSS

--- HDM/resources/views/collection.blade.php
+++ HDM/resources/views/collection.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
-    <link rel="stylesheet"  href="css/collection.css"/>
+    <link rel="stylesheet" href="{{asset('css/collection.css')}}">
 </head>
 <body>
 <div id="pageContainer">
