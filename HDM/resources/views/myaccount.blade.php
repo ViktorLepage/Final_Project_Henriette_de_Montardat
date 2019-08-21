e35188dcfab89c14cec152373a5498774f84a404
Fabrizio <fabriziolucenti@gmail.com>
2019-08-21 15:40:35 +0200

    updated footer v2.0

--- HDM/resources/views/myaccount.blade.php
+++ HDM/resources/views/myaccount.blade.php
@@ -13,7 +13,8 @@
     <title>Contact</title>
   </head>
   <body>
-
+<div id="pageContainer">
+    <div id="contentWrap">
     <header> @include('Layouts/headerTemplate')</header>
     <h1 class="pt-5">ACCOUNT INFORMATION</h1>
     <form id="theForm" action="/myaccount" method="POST" class="d-flex justify-content-center">
@@ -87,17 +88,18 @@
     </form>
 
 
-
-
-
-
+    </div>
+<footer id="footer">
+@include('Layouts/footerTemplate')
+</footer>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
-  @include('Layouts/footerTemplate')
+
 </html>
 
 
