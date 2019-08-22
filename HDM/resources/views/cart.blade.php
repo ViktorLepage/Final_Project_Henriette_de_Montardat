9fdb1cb17fbb074d52401ecf5d498173952a80d8
Gökdağ Göktepe <gokdag.goktepe@gmail.com>
2019-08-22 11:26:45 +0200

    Cart remove feature is done.

--- HDM/resources/views/cart.blade.php
+++ HDM/resources/views/cart.blade.php
@@ -12,11 +12,11 @@
 <body>
 
 
-<div id="pageContainer">
-    <div id="contentWrap">
-<header>
-@include('Layouts/headerTemplate')
-</header>
+    <div id="pageContainer">
+        <div id="contentWrap">
+    <header>
+    @include('Layouts/headerTemplate')
+    </header>
 
 
 
@@ -52,18 +52,18 @@
                     </div>
                 </div>
             @endforeach
-            <p class="cartTotal">TOTAL:<?php echo ' € ' . $total ; ?></p>
+            @if ($cart == null)
+                <h2>There is nothing to show</h2>
+                <p class="cartTotal">TOTAL:<?php echo ' € ' . $total ; ?></p>
+            @endif
         </div>
-
-    @elseif($cart == null)
-        <h2>There is nothing to show</h2>
     @endif
 
-</div>
-<footer id="footer">
-@include('Layouts/footerTemplate')
-</footer>
-</div>
+    </div>
+    <footer id="footer">
+    @include('Layouts/footerTemplate')
+    </footer>
+    </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
