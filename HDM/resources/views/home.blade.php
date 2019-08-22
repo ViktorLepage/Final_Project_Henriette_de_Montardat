8bdaf01698c0edb6da67f5330b4f571db31f7298
FabrizioLucenti <52154201+FabrizioLucenti@users.noreply.github.com>
2019-08-22 02:34:34 +0200

    little bit of responsiveness

--- HDM/resources/views/home.blade.php
+++ HDM/resources/views/home.blade.php
@@ -6,6 +6,7 @@
 <link href="https://fonts.googleapis.com/css?family=Satisfy&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,20 +17,35 @@
 <body>
     <div id="pageContainer">
         <div id="contentWrap">
-@include('layouts/headerTemplate')
-<div class="card mx-auto mb-5" style="width:450px">
-<img class="card-img-top" src="/images/homepagebackground4.jpg" alt="Card image">
-<div class="card-img-overlay ">
+            @include('layouts/headerTemplate')
+            <div class="card mx-auto mb-5" id="photo">
+                <img class="card-img-top" src="/images/homepagebackground4.jpg" alt="Card image">
+                <div class="card-img-overlay">
 
-    <div class="cardtitle"><p class="card-text font-italic mb-3">Le petit monde d'Henriette de Montardat</p></div>
-    <div class="cardtext"><p class="card-text mb-3">Mes années d'expérience au service de la femme, des femmes, dans la haute joaillerie m'ont amené à créer Henriette de Montardat.</p></div>
-    <div class="cardtext"><p class="card-text mb-3">Je crée, développe, commercialise des bijoux & accessoires & propose tous les mois des coups de cœur sous forme de collection capsule en quantité limitée.</p></div>
-    <div class="cardtext"><p class="card-text mb-3">Je transforme vos bijoux de famille en pièce artistique, unique & moderne.</p></div>
-    <div class="cardtext"><p class="card-text mb-3">Mes créations se veulent comme des porte bonheur, des talismans portés avec joie, fierté & élégance. Je vous attends dans mon univers.</p></div>
-    <div class="cardtext"><p class="card-text mb-3">Sophie Rouxel</p></div>
-  </div>
-</div>
-  <!--
+                    <section id="photoText">
+                        <div class="cardtitle">
+                            <p class="card-text font-italic mb-3">Le petit monde d'Henriette de Montardat</p>
+                        </div>
+                        <div class="cardtext">
+                            <p class="card-text mb-3">Mes années d'expérience au service de la femme, des femmes, dans la haute joaillerie m'ont amené à créer Henriette de Montardat.</p>
+                        </div>
+                        <div class="cardtext">
+                            <p class="card-text mb-3">Je crée, développe, commercialise des bijoux & accessoires & propose tous les mois des coups de cœur sous forme de collection capsule en quantité limitée.</p>
+                        </div>
+                        <div class="cardtext">
+                            <p class="card-text mb-3">Je transforme vos bijoux de famille en pièce artistique, unique & moderne.</p>
+                        </div>
+                        <div class="cardtext">
+                            <p class="card-text mb-3">Mes créations se veulent comme des porte bonheur, des talismans portés avec joie, fierté & élégance. Je vous attends dans mon univers.</p>
+                        </div>
+                        <div class="cardtext">
+                            <p class="card-text mb-3">Sophie Rouxel</p>
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+            <!--
 <div id="style_homepage">
             <div><p class="titre_homepage"> Le petit monde d'Henriette de Montardat </p></div>
             <div><p class="paragraphe_homepage">Mes années d'expérience au service de la femme, des femmes, dans la haute joaillerie m'ont amené à créer Henriette de Montardat. Je crée, développe, commercialise des bijoux & accessoires & propose tous les mois des coups de cœur sous forme de collection capsule en quantité limitée. Je transforme vos bijoux de famille en pièce artistique, unique & moderne. Mes créations se veulent comme des porte bonheur, des talismans portés avec joie, fierté & élégance. Je vous attends dans mon univers. </p></div>
@@ -38,16 +54,17 @@
 </div>
 -->
 
-</div>
+        </div>
 
 
-</div>
+    </div>
 
-<footer>
-@include('layouts/footerTemplate')
-</footer>
+    <footer>
+        @include('layouts/footerTemplate')
+    </footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </html>
