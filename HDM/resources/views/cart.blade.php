7613b31caf2116f8ac7ac4571534a6d9d5910ea1
Gökdağ Göktepe <gokdag.goktepe@gmail.com>
2019-08-23 13:13:04 +0200

    Cart Ajax Call Update

--- HDM/resources/views/cart.blade.php
+++ HDM/resources/views/cart.blade.php
@@ -23,14 +23,14 @@
         <div class="container">
             @foreach($cart as $eachCart=>$value)
                 <div class="container mr-0">
-                    <div class="row">
+                    <div name="cartItem" class="row">
                         <div class="col-3 shadow  mb-5 bg-white rounded mobilepicturecart">
                             <img id="myImg" height="150px" width="150px" src="{{$value->image}}" alt="product image">
                         </div>
                         <div class="col-8 border-top border-bottom border-dark rightCart position-relative">
                             <div class="d-flex justify-content-between">
                                 <p>{{$value->product_name}}</p>
-                                <p>€ {{$value->price}}</p>
+                                <p class="itemPrice">€ {{$value->price}}</p>
                                 <?php $total  += $value->price;?>
                             </div>
                             <div>
@@ -38,11 +38,11 @@
                             </div>
                             <div class="position-absolute removeButton">
                                     <form action="" method="POST">
-                                    @csrf
-                                    <input name="removeFromCart" value="{{$value->id}}" type="hidden">
-                                    @if(Auth::check())
-                                    <button class="button1 button2" type="submit">remove</button>
-                                    @endif
+                                        @csrf
+                                        <input name="removeFromCart" value="{{$value->id}}" type="hidden">
+                                        @if(Auth::check())
+                                            <button class="button1 button2" type="submit">remove</button>
+                                        @endif
                                     </form>
                             </div>
                         </div>
@@ -66,8 +66,35 @@
   <img class="modal-content" id="img01">
 
 </div>
-
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script>
+
+$(function(){
+    $('button[type="submit"]').click(function(e){
+        e.preventDefault();
+        console.log(e.target.parentElement.querySelector('input[name="removeFromCart"]').value);
+        $.ajax({
+            data:    {
+            "_token": "{{ csrf_token() }}",
+            "removeFromCart": e.target.parentElement.querySelector('input[name="removeFromCart"]').value
+            },
+            url: '/cart',
+            type: 'POST',
+            success: function(result) {
+                console.log(result);
+                // $('#resultForm').html('<div class="green">'+result+'</div>');
+
+                e.target.parentElement.closest('div[name="cartItem"]').remove();
+                $('.cartTotal').html('<h2 class="cartTotal">TOTAL:<?php echo " € " . $total; ?></h2>')
+
+            },
+            error: function(err){
+                // Si une erreur AJAX se produit
+            }
+        });
+    });
+});
+
 // Get the modal
 var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
