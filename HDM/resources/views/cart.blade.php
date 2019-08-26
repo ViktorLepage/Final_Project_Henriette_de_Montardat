dbe10552d07dbb485edb31da69452469d7325c2e
Gökdağ Göktepe <gokdag.goktepe@gmail.com>
2019-08-26 11:14:11 +0200

    Stripe and Ajax Updates

--- HDM/resources/views/cart.blade.php
+++ HDM/resources/views/cart.blade.php
@@ -61,15 +61,16 @@
                 @if ($cart == null)
                 <h2>There is nothing to show</h2>
                 @endif
-
+                @if ($cart != null)
                 <form action="/stripe" method="post">
                     @csrf
                     <h3 class="cartTotal">TOTAL: € {{$total}} </h3>
                     {{-- Ajax remove functionality test(GOKDAG) --}}
                     {{-- <h3 class="cartTotal">Removed Price: € {{$itemPrice[0]->price ?? "0"}} </h3> --}}
+                    <input type="submit" id="pay" value="PAY">
+                </form>
+                @endif
             </div>
-            <input type="submit" id="pay" value="PAY">
-            </form>
             @endif
 
         </div>
@@ -87,32 +88,35 @@
         })
 
         // THIS AJAX CALL IS FOR REMOVE FEATURE IN CART PAGE (GOKDAG)
-        // $(function(){
-        //     $('button[type="submit"]').click(function(e){
-        //         e.preventDefault();
-        //         console.log(e.target.parentElement.querySelector('input[name="removeFromCart"]').value);
-        //         $.ajax({
-        //             data:    {
-        //             "_token": "{{ csrf_token() }}",
-        //             "removeFromCart": e.target.parentElement.querySelector('input[name="removeFromCart"]').value
-        //             },
-        //             url: '/cart',
-        //             type: 'POST',
-        //             success: function(result) {
-        //                 console.log(result);
-        //                 // $('#resultForm').html('<div class="green">'+result+'</div>');
-        //                 e.target.parentElement.closest('div[name="cartItem"]').remove();
-        //                 $('.cartTotal').html('<h3 class="cartTotal">TOTAL:<?php echo " € " . $total = $total - ($itemPrice[0]->price ?? "0"); ?></h3>');
-        //                 // $('#totalPrice').html('<h3 class="cartTotal">TOTAL:<?php echo " € " . $total; ?></h3>');
-        //                 //take the price from controller and substract it from total//
+        $(function(){
+            $('button[type="submit"]').click(function(e){
+                e.preventDefault();
+                console.log(e.target.parentElement.querySelector('input[name="removeFromCart"]').value);
+                $.ajax({
+                    data:    {
+                    "_token": "{{ csrf_token() }}",
+                    "removeFromCart": e.target.parentElement.querySelector('input[name="removeFromCart"]').value
+                    },
+                    url: '/cart',
+                    type: 'POST',
+                    success: function(result) {
+                        console.log(result[0].price);
+                        let $x = <?php echo $total;?>-result[0].price;
+                        console.log("result" +$x);
+
+                        // $('#resultForm').html('<div class="green">'+result+'</div>');
+                        e.target.parentElement.closest('div[name="cartItem"]').remove();
+                        $('.cartTotal').html('<h3 class="cartTotal">TOTAL:'+ $x+'</h3>');
+                        // $('#totalPrice').html('<h3 class="cartTotal">TOTAL:<?php echo " € " . $total; ?></h3>');
+                        //take the price from controller and substract it from total//
 
 
-//             },
-//             error: function(err){
-//                 // If there is any error...
-//             }
-//         });
-//     });
-// });
+            },
+            error: function(err){
+                // If there is any error...
+            }
+        });
+    });
+});
     </script>
 </body>
